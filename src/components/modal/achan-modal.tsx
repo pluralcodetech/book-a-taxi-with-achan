77586eb96a4b2b68949dee556c5ad24c06680180
samuelige <samuelige.iyanu@gmail.com>
2021-11-03 11:46:31 +0100

    complete one way trip section

--- src/components/modal/achan-modal.tsx
+++ src/components/modal/achan-modal.tsx
@@ -105,7 +105,7 @@ export class AchhanModal {
                   <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-400 leading-tight focus:outline-none focus:shadow-outline" type="time"  />
                 </div>
               </div>
-              <button class="text-center mt-6 w-full border-0 p-3 outline-none focus:outline-none custom-book-btn">Book Now</button>
+              <button type="button" class="text-center mt-6 w-full border-0 p-3 outline-none focus:outline-none custom-book-btn">Book Now</button>
             </form>
         )
       }
@@ -114,7 +114,78 @@ export class AchhanModal {
       let oneWayContent = <slot/>
       if(this.showTripsContent) {
         oneWayContent = (
-          <div></div>
+          <form class="px-4 pt-4 pb-10">
+            <div class="flex justify-between">
+              <div class="customInput">
+                <label class="block text-gray-400 text-sm font-light mb-2">Firstname</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-400 leading-tight focus:outline-none focus:shadow-outline" type="text"  />
+              </div>
+              <div class="customInput">
+                <label class="block text-gray-400 text-sm font-light mb-2">Surname</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-400 leading-tight focus:outline-none focus:shadow-outline" type="text"  />
+              </div>
+            </div>
+
+            <div class="flex justify-between mt-4">
+              <div class="customInput">
+                <label class="block text-gray-400 text-sm font-light mb-2">Phone Number</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-400 leading-tight focus:outline-none focus:shadow-outline" type="text"  />
+              </div>
+              <div class="customInput">
+                <label class="block text-gray-400 text-sm font-light mb-2">Email Address</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-400 leading-tight focus:outline-none focus:shadow-outline" type="email"  />
+              </div>
+            </div>
+            <div class="flex justify-between mt-4">
+              <div class="customInput">
+                <label class="block text-gray-400 text-sm font-light mb-2">From</label>
+                <div class="relative w-full">
+                  <div class="pointer-events-none text-gray-600 absolute xl:mt-3 xl:ml-64  ">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon cursor-pointer icon-tabler icon-tabler-chevron-down" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z"></path>
+                        <polyline points="6 9 12 15 18 9"></polyline>
+                    </svg>
+                    
+                  </div>
+                  <select class='shadow appearance-none border rounded w-full py-2 px-3 text-gray-600 leading-tight focus:outline-none focus:shadow-outline' >
+                    <option value="" selected disabled hidden>select branch </option>
+                    <option>Bread</option>
+                    <option>Rice</option>
+                  </select>
+                </div>
+              </div>
+              <div class="customInput">
+                <label class="block text-gray-400 text-sm font-light mb-2">Destination</label>
+                <div class="relative w-full">
+                  <div class="pointer-events-none text-gray-600 absolute xl:mt-3 xl:ml-64  ">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon cursor-pointer icon-tabler icon-tabler-chevron-down" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z"></path>
+                        <polyline points="6 9 12 15 18 9"></polyline>
+                    </svg>
+                    
+                  </div>
+                  <select class='shadow appearance-none border rounded w-full py-2 px-3 text-gray-600 leading-tight focus:outline-none focus:shadow-outline' >
+                    <option value="" selected disabled hidden>select branch </option>
+                    <option>Bread</option>
+                    <option>Rice</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div class="flex justify-between mt-4">
+              <div class="customInput">
+                <label class="block text-gray-400 text-sm font-light mb-2">Date</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-400 leading-tight focus:outline-none focus:shadow-outline" type="date"  />
+              </div>
+              <div class="customInput">
+              <label class="block text-gray-400 text-sm font-light mb-2">Time</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-400 leading-tight focus:outline-none focus:shadow-outline" type="time"  />
+              </div>
+            </div>
+            
+            <button type="button" class="text-center mt-10 w-full border-0 p-3 outline-none focus:outline-none custom-book-btn">Book Now</button>
+          </form>
+        
         )
       }
 
@@ -138,7 +209,7 @@ export class AchhanModal {
                         {/*body*/}
 
                         {/* Tab Section */}
-                        <section id="tabs" class='p-4 flex justify-between w-full'>
+                        <section id="tabs" class='px-4 mt-10 mb-2 flex justify-between w-full'>
                             <button 
                                 onClick={this.onContentChange.bind(this, 'oneWay' )} 
                                 class={!this.showTripsContent ? "text-center border-2 p-3 outline-none focus:outline-none custom-tabs-btn custom-active" 
